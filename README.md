13fb6823bf1df76cf56eda819f19a17cbb38a437
saeed rasooli <saeed@rasooli>
2023-01-22 18:09:14 +0330

    create file and insert product to it

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## About parspack
 
 this is a product handler for creating a product and assigning reviews to it,
+
+
+sudo mkdir /opt/myprogram
+sudo vim /opt/myprogram/product_comments
+sudo chmod 777 /opt/myprogram/product_comments
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,13 +17,27 @@ class CommentController extends Controller
 
     public function create(CommentRequest $request)
     {
-        $product = ['name' => $request->get('p_name'), 'user_id' => Auth::id()];
+        $pName = $request->get('p_name');
+        $product = ['name' => $pName, 'user_id' => Auth::id()];
         $result = (new ProductController())->create(ProductDto::fromArray($product));
         $comment = new Comment();
         $comment->comment = $request->comment;
         $comment->user_id = Auth::id();
         $comment->product_id = $result->id;
         $comment->save();
+
+        $fileName = "/opt/myprogram/product_comments";
+        $output =  shell_exec("awk -F':' '$1 ==\"" . $pName . "\"{print $2}' " . $fileName);
+        $oparray = preg_split('/\s+/', trim($output));
+        $lastValue = $oparray[count($oparray) - 1];
+
+        if ($lastValue == null ) {
+        shell_exec("echo " . $pName . ": " . 1 . " >> " . $fileName);
+        } else {
+            shell_exec("sudo -S sed -i.bkp \"\/" . $pName . "\"\/d  $fileName");
+            shell_exec("echo " . $pName . ": " . $lastValue + 1 . " >> " . $fileName);
+        }
+
         return response()->json([
             'status' => 'success',
             'comment' => $comment,
