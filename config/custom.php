8686b4a3f1f71dfadec0595efa7ebb8314a2edb4
saeed rasooli <saeed@rasooli>
2023-01-23 02:00:43 +0330

    create unit test for comment limitter rule

--- config/custom.php
+++ config/custom.php
@@ -2,5 +2,5 @@
 
 return [
     "product_comment_counter_file" => "/opt/myprogram/product_comments",
-    "comment_limit_count" => 10000,
+    "comment_limit_count" => 1,
     ];
---
+++ tests/Unit/Rules/CommentLimitterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit\Rules;
+
+use App\Models\User;
+use App\Rules\CommentLimitter;
+use Symfony\Component\VarDumper\Cloner\Stub;
+use Tests\TestCase;
+
+class CommentLimitterTest extends TestCase
+{
+    /**
+     * @group unit
+     * @dataProvider productNameDataProvider
+     */
+    public function testPassesShouldWork(string|array|int|object $productName, bool $expected): void
+    {
+        //arrange
+        $rule = new CommentLimitter();
+
+        $user = User::find(81);
+        $this->be($user);
+
+        //act
+        $actual = $rule->passes('nothing', $productName);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function productNameDataProvider(): array
+    {
+        return [
+            'should work' => [
+                'product',
+                true
+            ],
+            'should work again' => [
+                'product',
+                true
+            ],
+            'should not work' => [
+                'product3',
+                false
+            ],
+        ];
+    }
+}
